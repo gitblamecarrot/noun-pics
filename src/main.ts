7a363959d8e236e48c066869df8e03e6760164f0
devcarrot <devcarrot@protonmail.com>
2022-10-04 11:09:17 -0600

    Add CORS header

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  app.enableCors();
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   await app.listen(3000);
